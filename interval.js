28b916c594779cf601b6ad39c4afc2bf3f9aa739
Chamak Saha <chamaksaha58@gmail.com>
2024-10-09 10:26:03 +0600

    practiced setInterval and clearInterval

---
+++ interval.js
@@ -0,0 +1,29 @@
+// console.log(1);
+// console.log(2);
+// // 1 parameter
+// // setTimeout(() => {
+// //     console.log(3);
+// // })
+
+// // 2 parameter
+// setTimeout(() => {
+//     console.log(3);
+// }, 3000);//1000 miliseconds = 1 second
+
+// console.log(4);
+// console.log(5);
+// console.log(6);
+
+let num = 0;
+const clockId = setInterval(() => {
+    num++;
+    // num = num + 1;
+    // num += 1;
+
+    //clearInterval(clockId);
+    //with condition:
+    if(num >= 5){
+        clearInterval(clockId);
+    }
+    console.log(clockId, num)
+}, 2000)
